cf962933b4457b46bb722cde809bf44f4cc38677
HaroldAguirre <haguirre@rdscr.com>
2022-08-20 19:18:39 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/contabilidad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.contabilidad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,6 @@
+import "package:flutter/material.dart";
+
+class AppRoutes {
+  static const String initalRoute = "home";
+  static final menuOptions = [];
+}
