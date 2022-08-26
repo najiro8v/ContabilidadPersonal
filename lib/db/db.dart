261ab109a332c796d8334e8b610037e4d782dcbc
Harold_Aguirre <haguirre@rdscr.com>
2022-08-26 14:22:21 -0600

    added idb_Sql

--- lib/db/db.dart
+++ lib/db/db.dart
@@ -1,36 +1,42 @@
+import 'package:idb_sqflite/idb_sqflite.dart';
 import "package:sqflite/sqflite.dart";
 import "package:contabilidad/models/models.dart";
 import "package:path/path.dart";
+import "package:idb_shim/idb.dart" as idb;
 
 class DatabaseSQL {
-  static Future<Database> _openDb() async {
-    return openDatabase(join(await getDatabasesPath(), 'gastos.db'),
-        onCreate: (db, version) {
-      return db.execute(
-          "CREATE TABLE finance(id INTEGER PRIMARY KEY, name TEXT, price REAL, desc TEXT,key TEXT)");
-    }, version: 1);
+  static const String _nameDb = "gastos.db";
+  static Future<idb.Database> _openDb() async {
+    var factory = getIdbFactorySqflite(databaseFactory);
+    return factory.open(
+      _nameDb,
+      version: 1,
+      onUpgradeNeeded: (VersionChangeEvent event) {
+        idb.Database db = event.database;
+      },
+    );
   }
 
   static Future<void> insert(ExpensesAndFinance expenses) async {
-    Database database = await _openDb();
-    database.insert("finance", expenses.toMap());
+    idb.Database db = await _openDb();
+    idb.Transaction tx = db.transaction(_nameDb, idbModeReadWrite);
+    idb.ObjectStore store = tx.objectStore(_nameDb);
   }
 
   static Future<void> delete(ExpensesAndFinance expenses) async {
     Database database = await _openDb();
-    database.delete("finance", where: "id = ?", whereArgs: [expenses.id]);
+    idb.delete("finance", where: "id = ?", whereArgs: [expenses.id]);
   }
 
   static Future<void> update(ExpensesAndFinance expenses) async {
     Database database = await _openDb();
-    database.update("finance", expenses.toMap(),
+    idb.update("finance", expenses.toMap(),
         where: "id = ?", whereArgs: [expenses.id]);
   }
 
   static Future<List<ExpensesAndFinance>> getAll() async {
     Database database = await _openDb();
-    final List<Map<String, dynamic>> expensesMap =
-        await database.query("finance");
+    final List<Map<String, dynamic>> expensesMap = await idb.query("finance");
 
     return List.generate(
         expensesMap.length,
@@ -44,7 +50,7 @@ class DatabaseSQL {
 
   static Future<void> dropResest() async {
     Database database = await _openDb();
-    await database.rawQuery(
+    await idb.rawQuery(
         " CREATE TABLE finance (id INTEGER PRIMARY KEY, name TEXT, price REAL, desc TEXT,key TEXT)");
   }
 }
