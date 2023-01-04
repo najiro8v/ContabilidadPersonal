f6a7b26c0ada7a721a924f5a7d9f23181f75b4c5
Harold_Aguirre <haguirre@rdscr.com>
2023-01-04 16:12:32 -0600

    comment before merge

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+//comment before the merge
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
