69a405bc6094f96734d6c6617068fc6ea3b61c6c
Harold_Aguirre <haguirre@rdscr.com>
2023-01-04 16:56:25 -0600

    cleaning app

--- lib/screens/entries_screen.dart
+++ lib/screens/entries_screen.dart
@@ -1,7 +1,6 @@
 import 'package:contabilidad/provider/providers.dart';
 import 'package:contabilidad/widget/element_custom_edit_value.dart';
 import 'package:flutter/material.dart';
-import 'package:contabilidad/controllers/controller.dart';
 import 'package:contabilidad/widget/widget.dart';
 import 'package:contabilidad/models/models.dart';
 import 'package:provider/provider.dart';
@@ -16,28 +15,8 @@ class EntriesScreen extends StatefulWidget {
 class _EntriesScreenState extends State<EntriesScreen> {
   List<ValueEntry> listado = [];
   final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
-  Map<String, DateTime> formDate = {
-    "dateI": DateTime.now(),
-    "dateF": DateTime.now()
-  };
-
-  getEntrys({int toast = 0}) async {
-    listado = await ValueEntryController.get();
-    listado.sort((a, b) => a.categoryName!.compareTo(b.categoryName!));
-
-    final dateFilter = formDate["dateI"] ?? DateTime.now();
-    final m = dateFilter.month < 10
-        ? "0${dateFilter.toUtc().month}"
-        : dateFilter.month;
-    final d = dateFilter.toUtc().day < 10
-        ? "0${dateFilter.toUtc().day}"
-        : dateFilter.toUtc().day;
-
-    final today = DateTime.parse("${dateFilter.toUtc().year}-$m-$d 00:00:00Z");
-    listado =
-        listado.where((i) => i.date! >= today.millisecondsSinceEpoch).toList();
-    setState(() {});
-  }
+//#Inicio de Region Para CRUD Entrada
+  //Actualizar Registro
 
   Future<dynamic> updateFunction(obj, context) async {
     var id = obj["id"].toString();
@@ -54,26 +33,17 @@ class _EntriesScreenState extends State<EntriesScreen> {
     );
     return await dbP.updateValueEntry(updateValueEntry);
   }
+  //Eliminar Registro
 
   Future<dynamic> deleteFunction(id, context) async {
     final dbP = Provider.of<DbProvider>(context, listen: false);
     await dbP.deleteValueEntry(id);
   }
-
-  submit() async {
-    myFormKey.currentState!.save();
-    if (!myFormKey.currentState!.validate()) {
-      return;
-    }
-    await getEntrys();
-    if (!mounted) return;
-    Navigator.of(context).pop();
-  }
+//#Fin de Region Para CRUD Entrada
 
   @override
   void initState() {
     super.initState();
-    getEntrys();
   }
 
   @override
@@ -84,20 +54,23 @@ class _EntriesScreenState extends State<EntriesScreen> {
           // ignore: invalid_use_of_protected_member
           ?.setValue(provider.categoryEntries!.id);
     }
+
+    resetFilter() async {
+      if (provider.keyFormFieldDropE!.currentState != null) {
+        provider.keyFormFieldDropE!.currentState!.reset();
+        provider.categoryEntries = null;
+        provider.filterEntries = [];
+      }
+      provider.categorya = null;
+      await provider.getEntry();
+    }
+
+/*Region Screen */
     return Scaffold(
         appBar: AppBar(title: const Text("Mis Registros")),
         floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.filter_alt_off_outlined),
-            onPressed: () async {
-              if (provider.keyFormFieldDropE!.currentState != null) {
-                provider.keyFormFieldDropE!.currentState!.reset();
-                provider.categoryEntries = null;
-                provider.filterEntries = [];
-              }
-
-              provider.categorya = null;
-              await provider.getEntry();
-            }),
+            onPressed: resetFilter,
+            child: const Icon(Icons.filter_alt_off_outlined)),
         body: Column(children: [
           const Padding(
             padding: EdgeInsets.symmetric(horizontal: 15),
@@ -105,27 +78,32 @@ class _EntriesScreenState extends State<EntriesScreen> {
           ),
           provider.valueEntrysD2!.isEmpty
               ? const _NotList()
-              : Expanded(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemBuilder: (_, index) {
-                      provider.valueEntrysD2![index];
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        child: ElementCustomEditValueEntry(
-                            padding: 10,
-                            label: "lolo",
-                            obj: provider.valueEntrysD2![index],
-                            deleteFunction: deleteFunction,
-                            updateFunction: updateFunction),
-                      );
-                    },
-                    itemCount: provider.valueEntrysD2!.isEmpty
-                        ? 1
-                        : provider.valueEntrysD2!.length,
-                  ),
-                ),
+              : _listRegister(provider),
         ]));
+  }
+
+/*Return*/
+  Expanded _listRegister(DbProvider provider) {
+    return Expanded(
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemBuilder: (_, index) {
+          provider.valueEntrysD2![index];
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: ElementCustomEditValueEntry(
+                padding: 10,
+                label: "lolo",
+                obj: provider.valueEntrysD2![index],
+                deleteFunction: deleteFunction,
+                updateFunction: updateFunction),
+          );
+        },
+        itemCount: provider.valueEntrysD2!.isEmpty
+            ? 1
+            : provider.valueEntrysD2!.length,
+      ),
+    );
   }
 }
 
@@ -139,90 +117,32 @@ class _TitleFilter extends StatelessWidget {
     var provider = Provider.of<DbProvider>(context);
     provider.getEntry();
     provider.getCategorias();
+
+    var expanded = Expanded(
+        child: TextFormField(
+      decoration: const InputDecoration(
+          icon: Icon(Icons.search_sharp), labelText: "Busqueda"),
+    ));
+
     return Column(
       children: [
         Row(
           children: [
-            Expanded(
-                child: DropdownButtonFormField(
-                    key: provider.keyFormFieldDropE,
-                    decoration: const InputDecoration(labelText: "Categorias"),
-                    items: provider.categorias != null
-                        ? provider.categorias!
-                            .where((element) => element.enable!)
-                            .map((Category e) => DropdownMenuItem(
-                                  value: e.id,
-                                  child: Text(e.name!),
-                                ))
-                            .toList()
-                        : const [
-                            DropdownMenuItem(
-                              value: 0,
-                              child: Text("1"),
-                            ),
-                            DropdownMenuItem(
-                              value: 1,
-                              child: Text("2"),
-                            )
-                          ],
-                    onChanged: (value) async {
-                      await provider.setSubCategoriasFilter(value.toString());
-                    })),
+            _DropwdownFilter(),
             const SizedBox(
               width: 10,
             ),
-            Expanded(
-                child: TextFormField(
-              decoration: const InputDecoration(
-                  icon: Icon(Icons.search_sharp), labelText: "Busqueda"),
-            )),
+            expanded,
           ],
         ),
         const SizedBox(
           height: 10,
         ),
-        SizedBox(
-          height: 100,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: ((context, index) {
-              Entry entry = provider.categoryEntries != null
-                  ? provider.registrosEntries![index]
-                  : provider.registrosAll![index];
-              return Container(
-                width: 100,
-                key: Key(entry.key.toString()),
-                margin: const EdgeInsets.symmetric(horizontal: 5),
-                child: CheckFilter(
-                  filter: entry,
-                ),
-              );
-            }),
-            shrinkWrap: true,
-            itemCount: provider.categoryEntries != null
-                ? provider.registrosEntries!.length
-                : provider.registrosAll!.length,
-          ),
-        ),
+        _ListValueEntrys(),
         const SizedBox(
           height: 10,
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            TextButton.icon(
-                onPressed: () {},
-                icon: const Icon(Icons.filter_list),
-                label: const Text("Filtros")),
-            TextButton(
-                onPressed: () async {
-                  // await _openDatePicker();
-                  await provider.getValueEntries();
-                  await provider.setNewList();
-                },
-                child: const Text("Buscar"))
-          ],
-        ),
+        _ButtonFilter(),
         const SizedBox(
           height: 10,
         ),
@@ -250,5 +170,88 @@ class _NotList extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+
+class _ListValueEntrys extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<DbProvider>(context, listen: false);
+    return SizedBox(
+        height: 100,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: ((context, index) {
+            Entry entry = provider.categoryEntries != null
+                ? provider.registrosEntries![index]
+                : provider.registrosAll![index];
+            return Container(
+              width: 100,
+              key: Key(entry.key.toString()),
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              child: CheckFilter(
+                filter: entry,
+              ),
+            );
+          }),
+          shrinkWrap: true,
+          itemCount: provider.categoryEntries != null
+              ? provider.registrosEntries!.length
+              : provider.registrosAll!.length,
+        ));
+  }
+}
+
+class _ButtonFilter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<DbProvider>(context, listen: false);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        TextButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.filter_list),
+            label: const Text("Filtros")),
+        TextButton(
+            onPressed: () async {
+              await provider.getValueEntries();
+              await provider.setNewList();
+            },
+            child: const Text("Buscar"))
+      ],
+    );
+  }
+}
+
+class _DropwdownFilter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<DbProvider>(context, listen: false);
+    return Expanded(
+        child: DropdownButtonFormField(
+            key: provider.keyFormFieldDropE,
+            decoration: const InputDecoration(labelText: "Categorias"),
+            items: provider.categorias != null
+                ? provider.categorias!
+                    .where((element) => element.enable!)
+                    .map((Category e) => DropdownMenuItem(
+                          value: e.id,
+                          child: Text(e.name!),
+                        ))
+                    .toList()
+                : const [
+                    DropdownMenuItem(
+                      value: 0,
+                      child: Text("1"),
+                    ),
+                    DropdownMenuItem(
+                      value: 1,
+                      child: Text("2"),
+                    )
+                  ],
+            onChanged: (value) async {
+              await provider.setSubCategoriasFilter(value.toString());
+            }));
   }
 }
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,6 +1,5 @@
 export 'package:contabilidad/screens/finances_screen.dart';
 export 'package:contabilidad/screens/menu_screen.dart';
-export 'package:contabilidad/screens/deposit_screen.dart';
 export 'package:contabilidad/screens/data_screen.dart';
 export 'package:contabilidad/screens/settings/settings.dart';
 export 'category/category_add_screen.dart';
