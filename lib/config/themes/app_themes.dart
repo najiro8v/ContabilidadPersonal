93c8d014bdaef1fe4b63ed4d6fb29df1aa1ac023
Harold_Aguirre <haroldantonio@777gmail.com>
2023-09-26 19:49:45 -0600

    tema de app

--- lib/config/themes/app_themes.dart
+++ lib/config/themes/app_themes.dart
@@ -4,12 +4,17 @@ class AppTheme {
   static const Color primary = Colors.indigo;
   static Color secondary = Colors.indigo.shade100;
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    primaryColor: primary,
-    appBarTheme: const AppBarTheme(color: primary),
-    navigationBarTheme: const NavigationBarThemeData().copyWith(
-        elevation: 10,
-        backgroundColor: primary,
-        labelTextStyle: MaterialStateProperty.all(TextStyle(color: secondary)),
-        iconTheme: MaterialStateProperty.all(IconThemeData(color: secondary))),
-  );
+      primaryColor: primary,
+      appBarTheme: const AppBarTheme(color: primary),
+      navigationBarTheme: const NavigationBarThemeData().copyWith(
+          elevation: 10,
+          backgroundColor: primary,
+          labelTextStyle:
+              MaterialStateProperty.all(TextStyle(color: secondary)),
+          iconTheme:
+              MaterialStateProperty.all(IconThemeData(color: secondary))),
+      textButtonTheme: TextButtonThemeData(
+        style:
+            TextButton.styleFrom(primary: secondary, backgroundColor: primary),
+      ));
 }
